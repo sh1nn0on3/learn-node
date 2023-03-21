f57465ffeea92199639828ba56f713402ac0cda4
sh1nn0on3 <sanogaku31@gmail.com>
2023-03-21 23:05:53 +0700

    first

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-import express from "express";
-import cors from "cors";
-import {} from "dotenv/config";
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-
 app.use(express.json());
 
-app.get("/", cors(corsOptions), (req, res, next) => {
-  return res.send(`server on ...`);
+app.use("/", (req, res) => {
+  return res.send("server on ...");
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on the port ` + PORT);
+  console.log("server is running on the port " + PORT);
 });
